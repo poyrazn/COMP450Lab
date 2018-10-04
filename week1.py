5159bd06c8e72c27de1556689ae8b17097fc0df1
Nehir Poyraz <poyrazf@mef.edu.tr>
2018-10-04 21:43:48 +0300

    refinement

--- week1.py
+++ week1.py
@@ -12,21 +12,23 @@ import random
 class Player:
     id = 0
 
-    def __init__(self):
+    def __init__(self, randomness: bool = None):
         self.wincount = 0
         self.movecount = [0, 0, 0]
         self.action: str
         self.previous = None
+        if randomness:
+            self.israndom = randomness
         self.id = Player.id
         self.name = "Player"
         if self.id != 0:
             self.name += str(self.id)
         Player.id += 1
 
-    def act(self, israndom: bool):
+    def act(self):
         while True:
             try:
-                if israndom:
+                if self.israndom:
                     action = random.choice(actions)
                 else:
                     action = input("Make your move:\n[R]-[P]-[S]\n")
@@ -50,14 +52,12 @@ class Computer(Player):
         self.id = Computer.id
         if self.id != 0:
             self.name += str(self.id)
-
         Computer.id += 1
 
-
     def act(self):
-        if self.previous == 'w':
+        if self.previous == 1:
             index = (self.previndex + 1) % 3
-        elif self.previous == 'l':
+        elif self.previous == -1:
             index = (self.previndex - 1) % 3
         else:
             index = random.randint(0, 2)
@@ -83,21 +83,18 @@ def getstat(player1: Player, player2: Player):
     c = counter / 100
     for i in range(len(actions)):
         moves[i] = "{0:<3}{1:>5}{2:>2}{4:^10}{0:<3}{3:>5}{2:>2}".format(actions[i], (player1.movecount[i]/c), "%", (player2.movecount[i]/c), " ")
-    status = "{0:<20}{1:<20}\n {2}\n {3}\n {4}".format("Player moves", "Computer moves", *moves)
+    status = "{0:<20}{1:<20}\n {2}\n {3}\n {4}".format(player1.name + "moves", player2.name + "moves", *moves)
     print("\n")
     print(status)
     print("\n")
     with open('stats.txt') as stats:
         stat = stats.read()
     print(stat)
-    print("Player won", player1.wincount, "times")
-    print("Computer won", player2.wincount, "times")
+    print(player1.name, "won", player1.wincount, "times")
+    print(player2.name, "won", player2.wincount, "times")
 
 
 def checkstatus(player1: Player, player2: Player):
-
-    # player1: player
-    # player2: computer
 
     global winner, counter
 
@@ -143,11 +140,13 @@ def checkstatus(player1: Player, player2: Player):
             player2.wincount += 1
             player1.previous = -1
             player2.previous = 1
+
         elif player2.action == 'P':
             winner = player1
             player1.wincount += 1
             player1.previous = 1
             player2.previous = -1
+
         else:
             winner = None
             player1.previous = 0
@@ -155,19 +154,24 @@ def checkstatus(player1: Player, player2: Player):
     counter += 1
 
 
-def start(rand=False, loopcount=None):
-    player1 = Player()
+def start(rand=False, loopcount=None, aimatch=False):
+    if aimatch:
+        player1 = Computer()
+    else:
+        player1 = Player(rand)
     player2 = Computer()
     pushstat('w', player1.name, player2.name, 'WINNER')
     for i in range(loopcount):
-        loop(player1, player2, rand)
+        loop(player1, player2)
     getstat(player1, player2)
 
 
-def loop(player1: Player, player2: Player, rand: bool):
-    player1.act(rand)
+def loop(player1: Player, player2: Player):
+    player1.act()
     player2.act()
     checkstatus(player1, player2)
+    print(player1.name, "move:", player1.action)
+    print(player2.name, "move:", player2.action)
     if winner:
         pushstat('a', player1.action, player2.action, winner.name)
         print("The winner is", winner.name)
@@ -178,6 +182,6 @@ def loop(player1: Player, player2: Player, rand: bool):
 
 if __name__ == '__main__':
     rand = False
-    loopcount = 5
+    loopcount = 100
 
-    start(rand, loopcount)
+    start(rand, loopcount, aimatch=True)
